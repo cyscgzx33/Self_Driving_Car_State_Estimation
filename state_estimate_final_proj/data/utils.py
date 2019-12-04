531f575d283c571a146718d3a18ddd89cb82b2e8
Yusen Chen <yusen_chen@hra.com>
2019-12-04 15:16:44 -0500

    feat: updated the final proj files

--- state_estimate_final_proj/data/utils.py
+++ state_estimate_final_proj/data/utils.py
@@ -1,5 +1,19 @@
+# Utility classes and functions for Coursera SDC Course 2.
+#
+# Authors: Trevor Ablett and Jonathan Kelly
+# University of Toronto Institute for Aerospace Studies
+
 import numpy as np
 from numpy import sin, cos, arctan2, sqrt
+
+class StampedData():
+    def __init__(self):
+        self.data = []
+        self.t = []
+
+    def convert_lists_to_numpy(self):
+        self.data = np.array(self.data)
+        self.t = np.array(self.t)
 
 def to_rot(r):
     Rx = np.mat([[ 1,         0,           0],
@@ -17,22 +31,18 @@ def to_rot(r):
     return Rz*Ry*Rx
 
 def to_mat(p, r):
-    "Given a position [m] and orientation in RPY [rad], create homogenous transformation matrix"
+    "Given a position [m] and orientation as RPY [rad], create homogenous transformation matrix."
     R = to_rot(r)
-
     return np.mat(np.r_[np.c_[R, p], [np.array([0, 0, 0, 1])]])
 
 def from_mat(T):
-    "Get position [m] and orientation in RPY [rad] from homogenous transformation matrix"
-
+    "Get position [m] and orientation as RPY [rad] from homogenous transformation matrix."
     p = [T[0,3], T[1,3], T[2,3]]
-
     r = [arctan2(T[2,1],T[2,2]) , arctan2(-T[2,0],sqrt(T[2,1] ** 2 + T[2,2] ** 2)), arctan2(T[1,0],T[0,0]) ]
-
     return p, r
 
-def transform_data(p, r, T_frame):
-    "Transform gt data to different frame"
+def transform_data_right(p, r, T_frame):
+    "Transform data to a different frame."
     p_new = [0]*len(p)
     r_new = [0]*len(p)
 
@@ -44,7 +54,7 @@ def transform_data(p, r, T_frame):
     return np.array(p_new), np.array(r_new)
 
 def transform_data_left(p, r, T_frame):
-    "Transform gt data to different frame"
+    "Transform data to different frame."
     p_new = [0]*len(p)
     r_new = [0]*len(p)
 
@@ -55,7 +65,7 @@ def transform_data_left(p, r, T_frame):
 
     return np.array(p_new), np.array(r_new)
 
-def to_own_frame(r,x):
+def to_own_frame(r, x):
     x_new = np.zeros(x.shape)
 
     for i in (range(len(x))):
@@ -63,7 +73,25 @@ def to_own_frame(r,x):
 
     return x_new
 
-def intg(x,t):
+def to_angular_rates(r, r_dot):
+    """
+    Compute the inverse of the Euler kinematical matrix for the given roll,
+    pitch, and yaw angles - the kinematical matrix is used to compute the
+    rate of change of the Euler angles as a function of the angular velocity.
+    """
+    # Still need to differentiate the Euler angles first wrt time.
+    cr = np.cos(r[0])
+    sr = np.sin(r[0])
+    sp = np.sin(r[1])
+    cp = np.cos(r[1])
+
+    # Generate inverse of kinematical matrix.
+    # M = np.array([[1, tp*sr, tp*cr], [0, cr, -sr], [0, sr/cp, cr/cp]])
+    G = np.array([[1, 0, -sp], [0, cr, sr*cp], [0, -sr, cr*cp]])
+
+    return G @ r_dot
+
+def integ(x, t):
     out = [None] * (len(x) + 1)
     for i in range(len(out)):
         dt = t[i + 1] - t[i]
@@ -74,6 +102,7 @@ def intg(x,t):
 def diff(x, t):
     out = [None] * (len(x) - 1)
     for i in (range(len(out))):
+        # Forward finite difference scheme.
         dt = t[i + 1] - t[i]
         dx = x[i + 1, :] - x[i, :]
         out[i] = dx / dt
--- state_estimate_final_proj/es_ekf.py
+++ state_estimate_final_proj/es_ekf.py
@@ -1,24 +1,20 @@
-# Main code for the Coursera SDC Course 2 final project
+# Starter code for the Coursera SDC Course 2 final project.
 #
-# Author: Trevor Ablett
+# Author: Trevor Ablett and Jonathan Kelly
 # University of Toronto Institute for Aerospace Studies
-
 import pickle
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-from rotations import Quaternion, skew_symmetric
-sys.path.append('./data')
+from rotations import angle_normalize, rpy_jacobian_axis_angle, skew_symmetric, Quaternion
 
 #### 1. Data ###################################################################################
 
 ################################################################################################
 # This is where you will load the data from the pickle files. For parts 1 and 2, you will use
-# p1_data.pkl. For part 3, you will use p3_data.pkl.
+# p1_data.pkl. For Part 3, you will use pt3_data.pkl.
 ################################################################################################
-with open('data/p1_data.pkl', 'rb') as file:
+with open('data/pt1_data.pkl', 'rb') as file:
     data = pickle.load(file)
 
 ################################################################################################
@@ -67,31 +63,31 @@ plt.show()
 
 ################################################################################################
 # Remember that our LIDAR data is actually just a set of positions estimated from a separate
-# scan-matching system, so we can just insert it into our solver as another position
-# measurement, just as we do for GNSS. However, the LIDAR frame is not the same as the frame
-# shared by the IMU and the GNSS. To remedy this, we transform the LIDAR data to the IMU frame
-# using our known extrinsic calibration rotation matrix C_li and translation vector t_li_i.
+# scan-matching system, so we can insert it into our solver as another position measurement,
+# just as we do for GNSS. However, the LIDAR frame is not the same as the frame shared by the
+# IMU and the GNSS. To remedy this, we transform the LIDAR data to the IMU frame using our 
+# known extrinsic calibration rotation matrix C_li and translation vector t_i_li.
 #
 # THIS IS THE CODE YOU WILL MODIFY FOR PART 2 OF THE ASSIGNMENT.
 ################################################################################################
-# This is the correct calibration rotation matrix, corresponding to an euler rotation of 0.05, 0.05, .1.
+# Correct calibration rotation matrix, corresponding to Euler RPY angles (0.05, 0.05, 0.1).
 C_li = np.array([
-    [ 0.99376, -0.09722,  0.05466],
-    [ 0.09971,  0.99401, -0.04475],
-    [-0.04998,  0.04992,  0.9975 ]
+   [ 0.99376, -0.09722,  0.05466],
+   [ 0.09971,  0.99401, -0.04475],
+   [-0.04998,  0.04992,  0.9975 ]
 ])
 
-# This is an incorrect calibration rotation matrix, corresponding to a rotation of 0.05, 0.05, 0.05
+# Incorrect calibration rotation matrix, corresponding to Euler RPY angles (0.05, 0.05, 0.05).
 # C_li = np.array([
-#     [ 0.9975 , -0.04742,  0.05235],
-#     [ 0.04992,  0.99763, -0.04742],
-#     [-0.04998,  0.04992,  0.9975 ]
+#      [ 0.9975 , -0.04742,  0.05235],
+#      [ 0.04992,  0.99763, -0.04742],
+#      [-0.04998,  0.04992,  0.9975 ]
 # ])
 
-t_li_i = np.array([0.5, 0.1, 0.5])
+t_i_li = np.array([0.5, 0.1, 0.5])
 
-lidar.data = (C_li @ lidar.data.T).T + t_li_i
-
+# Transform from the LIDAR frame to the vehicle (IMU) frame.
+lidar.data = (C_li @ lidar.data.T).T + t_i_li
 
 #### 2. Constants ##############################################################################
 
@@ -100,10 +96,10 @@ lidar.data = (C_li @ lidar.data.T).T + t_li_i
 # most important aspects of a filter is setting the estimated sensor variances correctly.
 # We set the values here.
 ################################################################################################
-var_imu_f = 0.1
-var_imu_w = 1.0
-var_gnss = 0.01
-var_lidar = 0.25
+var_imu_f = 0.10
+var_imu_w = 0.25
+var_gnss  = 0.01
+var_lidar = 1.00
 
 ################################################################################################
 # We can also set up some constants that won't change for any iteration of our solver.
@@ -113,7 +109,6 @@ l_jac = np.zeros([9, 6])
 l_jac[3:, :] = np.eye(6)  # motion model noise jacobian
 h_jac = np.zeros([3, 9])
 h_jac[:, :3] = np.eye(3)  # measurement model jacobian
-
 
 #### 3. Initial Values #########################################################################
 
@@ -125,14 +120,13 @@ v_est = np.zeros([imu_f.data.shape[0], 3])  # velocity estimates
 q_est = np.zeros([imu_f.data.shape[0], 4])  # orientation estimates as quaternions
 p_cov = np.zeros([imu_f.data.shape[0], 9, 9])  # covariance matrices at each timestep
 
-# Set initial values
+# Set initial values.
 p_est[0] = gt.p[0]
 v_est[0] = gt.v[0]
 q_est[0] = Quaternion(euler=gt.r[0]).to_numpy()
-p_cov[0] = np.eye(9)  # covariance of estimate
-gnss_i = 0
+p_cov[0] = np.zeros(9)  # covariance of estimate
+gnss_i  = 0
 lidar_i = 0
-
 
 #### 4. Measurement Update #####################################################################
 
@@ -149,8 +143,7 @@ def measurement_update(sensor_var, p_cov_check, y_k, p_check, v_check, q_check):
 
     # 3.4 Compute corrected covariance
 
-    return p_check, v_check, q_check, p_cov_check
-
+    return p_hat, v_hat, q_hat, p_cov_hat
 
 #### 5. Main Filter Loop #######################################################################
 
@@ -163,12 +156,13 @@ for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial predic
 
     # 1. Update state with IMU inputs
 
-    # 1.1 Linearize Motion Model
+    # 1.1 Linearize the motion model and compute Jacobians
 
     # 2. Propagate uncertainty
 
     # 3. Check availability of GNSS and LIDAR measurements
 
+    # Update states (save)
 
 #### 6. Results and Analysis ###################################################################
 
@@ -182,46 +176,62 @@ est_traj_fig = plt.figure()
 ax = est_traj_fig.add_subplot(111, projection='3d')
 ax.plot(p_est[:,0], p_est[:,1], p_est[:,2], label='Estimated')
 ax.plot(gt.p[:,0], gt.p[:,1], gt.p[:,2], label='Ground Truth')
-ax.set_xlabel('x [m]')
-ax.set_ylabel('y [m]')
-ax.set_zlabel('z [m]')
-ax.set_title('Final Estimated Trajectory')
-ax.legend()
-ax.set_zlim(-1, 5)
+ax.set_xlabel('Easting [m]')
+ax.set_ylabel('Northing [m]')
+ax.set_zlabel('Up [m]')
+ax.set_title('Ground Truth and Estimated Trajectory')
+ax.set_xlim(0, 200)
+ax.set_ylim(0, 200)
+ax.set_zlim(-2, 2)
+ax.set_xticks([0, 50, 100, 150, 200])
+ax.set_yticks([0, 50, 100, 150, 200])
+ax.set_zticks([-2, -1, 0, 1, 2])
+ax.legend(loc=(0.62,0.77))
+ax.view_init(elev=45, azim=-50)
 plt.show()
 
 ################################################################################################
 # We can also plot the error for each of the 6 DOF, with estimates for our uncertainty
 # included. The error estimates are in blue, and the uncertainty bounds are red and dashed.
-# The uncertainty bounds are +/- 3 standard deviations based on our uncertainty.
+# The uncertainty bounds are +/- 3 standard deviations based on our uncertainty (covariance).
 ################################################################################################
 error_fig, ax = plt.subplots(2, 3)
-error_fig.suptitle('Error plots')
+error_fig.suptitle('Error Plots')
 num_gt = gt.p.shape[0]
 p_est_euler = []
+p_cov_euler_std = []
 
 # Convert estimated quaternions to euler angles
-for q in q_est:
-    p_est_euler.append(Quaternion(*q).to_euler())
+for i in range(len(q_est)):
+    qc = Quaternion(*q_est[i, :])
+    p_est_euler.append(qc.to_euler())
+
+    # First-order approximation of RPY covariance
+    J = rpy_jacobian_axis_angle(qc.to_axis_angle())
+    p_cov_euler_std.append(np.sqrt(np.diagonal(J @ p_cov[i, 6:, 6:] @ J.T)))
+
 p_est_euler = np.array(p_est_euler)
+p_cov_euler_std = np.array(p_cov_euler_std)
 
 # Get uncertainty estimates from P matrix
-p_cov_diag_std = np.sqrt(np.diagonal(p_cov, axis1=1, axis2=2))
+p_cov_std = np.sqrt(np.diagonal(p_cov[:, :6, :6], axis1=1, axis2=2))
 
-titles = ['x', 'y', 'z', 'x rot', 'y rot', 'z rot']
+titles = ['Easting', 'Northing', 'Up', 'Roll', 'Pitch', 'Yaw']
 for i in range(3):
     ax[0, i].plot(range(num_gt), gt.p[:, i] - p_est[:num_gt, i])
-    ax[0, i].plot(range(num_gt), 3 * p_cov_diag_std[:num_gt, i], 'r--')
-    ax[0, i].plot(range(num_gt), -3 * p_cov_diag_std[:num_gt, i], 'r--')
+    ax[0, i].plot(range(num_gt),  3 * p_cov_std[:num_gt, i], 'r--')
+    ax[0, i].plot(range(num_gt), -3 * p_cov_std[:num_gt, i], 'r--')
     ax[0, i].set_title(titles[i])
+ax[0,0].set_ylabel('Meters')
 
 for i in range(3):
-    ax[1, i].plot(range(num_gt), gt.r[:, i] - p_est_euler[:num_gt, i])
-    ax[1, i].plot(range(num_gt), 3 * p_cov_diag_std[:num_gt, i+6], 'r--')
-    ax[1, i].plot(range(num_gt), -3 * p_cov_diag_std[:num_gt, i+6], 'r--')
+    ax[1, i].plot(range(num_gt), \
+        angle_normalize(gt.r[:, i] - p_est_euler[:num_gt, i]))
+    ax[1, i].plot(range(num_gt),  3 * p_cov_euler_std[:num_gt, i], 'r--')
+    ax[1, i].plot(range(num_gt), -3 * p_cov_euler_std[:num_gt, i], 'r--')
     ax[1, i].set_title(titles[i+3])
+ax[1,0].set_ylabel('Radians')
 plt.show()
-
 
 #### 7. Submission #############################################################################
 
